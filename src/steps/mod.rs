4c916414f690080043cd2bcbe8cd61fae85a6742
Kennan Hunter <kennanhunter5@gmail.com>
2022-08-07 15:00:18 -0400

    fix annoying warning

--- src/steps/mod.rs
+++ src/steps/mod.rs
@@ -1,27 +1,27 @@
 mod crop;
 
-use std::fmt::Error;
-
 use crate::schema::mutation::steps::Step;
+use juniper::{graphql_value, FieldError};
 use photon_rs::PhotonImage;
 
 use self::crop::crop_image;
 
 /// Applies the specified step to the image
-pub fn step_to_func(step: Step, image: &mut PhotonImage) -> Result<PhotonImage, Error> {
-    match step {
-        crop => {
-            let crop = crop.crop.unwrap(); // Bruh
-            return Ok(crop_image(
-                image,
-                crop.height,
-                crop.width,
-                crop.xoffset,
-                crop.yoffset,
-            ));
-        }
-        _ => {
-            panic!("Supply a valid step method")
-        }
+pub fn step_to_func(step: Step, image: &mut PhotonImage) -> Result<PhotonImage, FieldError> {
+    if step.crop.is_some() {
+        let crop = step.crop.unwrap(); // Bruh
+        return Ok(crop_image(
+            image,
+            crop.height,
+            crop.width,
+            crop.xoffset,
+            crop.yoffset,
+        ));
     }
+
+    // If no step is valid then panic
+    Err(FieldError::new(
+        "No valid step specified",
+        graphql_value!({ "internal_error": "No step specified" }),
+    ))
 }
